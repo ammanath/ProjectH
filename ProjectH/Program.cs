0747101f305f5d0afab9b34dda67e9ddb1f119a5
brijesh <github@ammanath.com>
2021-05-28 22:19:19 +0530

    Add project files.

---
+++ ProjectH/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+
+
+namespace ProjectH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string guess;
+
+            Thread t = new Thread(Print1);
+            t.Start();
+
+            Random rnd = new Random();
+            int genNext;
+            for (int i = 0; i < 100; i++)
+            {
+                genNext = rnd.Next(30, 700);
+                Thread.Sleep(100);
+                Console.Write(genNext);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Print1()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(100);
+                Console.Write('X');
+            }
+            
+        }
+    }
+}
